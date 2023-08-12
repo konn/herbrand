ec8b8f047453f179cf5c89c6bb34ec85830913e3
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-12 12:30:31 +0900

    ci: test case for solve

--- test/Logic/Propositional/Classical/SAT/TableauxSpec.hs
+++ test/Logic/Propositional/Classical/SAT/TableauxSpec.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TupleSections #-}
 
-module Logic.Propositional.Classical.SAT.TableauxSpec where
+module Logic.Propositional.Classical.SAT.TableauxSpec (
+  test_prove,
+  test_solve,
+) where
 
 import qualified Control.Foldl as L
-import Control.Monad (when)
 import Data.Maybe (fromMaybe)
 import Logic.Propositional.Classical.SAT.BruteForce
 import Logic.Propositional.Classical.SAT.Tableaux
+import Logic.Propositional.Classical.SAT.Types
 import Logic.Propositional.Classical.SAT.Types (eval)
 import Logic.Propositional.Classical.Syntax.TestUtils
 import Logic.Propositional.Syntax.General
@@ -90,5 +93,81 @@ test_prove =
             assert $
               P.eq
                 .$ ("expected", Just False)
+                .$ ("answer", eval m phi')
+    ]
+
+test_solve :: TestTree
+test_solve =
+  testGroup
+    "solve"
+    [ testProperty "Gives a correct answer" $ do
+        arity <- gen $ F.integral $ F.between (1, 5)
+        F.collect "arity" [arity]
+
+        size <- gen $ F.integral $ F.between (1, 128)
+        F.collect "size" [size]
+
+        phi <- gen $ fullFormula arity size
+        F.collect
+          "# of maximum occurrence"
+          [ L.fold
+              ( L.premap (,1 :: Int) $
+                  L.fold (fromMaybe 0 <$> L.maximum) <$> L.foldByKeyMap L.sum
+              )
+              phi
+          ]
+        F.label
+          "consistency"
+          [ case classifyFormula phi of
+              Tautology -> "Tautology"
+              Consistent {} -> "Satsifiable"
+              Inconsistent -> "Unsatisfiable"
+          ]
+        let ans = solve phi
+        case classifyFormula phi of
+          Inconsistent ->
+            assert $
+              P.eq .$ ("expected", Unsat) .$ ("answer", ans)
+          _ ->
+            assert $
+              P.satisfies
+                ("Satisfiable", \case Satisfiable {} -> True; _ -> False)
+                .$ ("answer", ans)
+    , testProperty "Gives a correct model" $ do
+        arity <- gen $ F.integral $ F.between (1, 5)
+        F.collect "arity" [arity]
+
+        size <- gen $ F.integral $ F.between (1, 128)
+        F.collect "size" [size]
+
+        phi <- gen $ fullFormula arity size
+        F.collect
+          "# of maximum occurrence"
+          [ L.fold
+              ( L.premap (,1 :: Int) $
+                  L.fold (fromMaybe 0 <$> L.maximum) <$> L.foldByKeyMap L.sum
+              )
+              phi
+          ]
+        let consis = classifyFormula phi
+
+        F.label
+          "consistency"
+          [ case consis of
+              Tautology -> "Tautology"
+              Consistent {} -> "Satsifiable"
+              Inconsistent -> "Unsatisfiable"
+          ]
+        let phi' = case consis of
+              Inconsistent -> Not NoExtField phi
+              _ -> phi
+        info $ "Satsifiable Formula: " <> show phi'
+        case solve phi' of
+          Unsat -> testFailed "Expected Satisfiable, but got Unsat"
+          Satisfiable m -> do
+            info $ "Given model: " <> show m
+            assert $
+              P.eq
+                .$ ("expected", Just True)
                 .$ ("answer", eval m phi')
     ]
