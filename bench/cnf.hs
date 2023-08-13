567cfce0b42b2eeed38cfa7056578fefbd932963
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-14 00:43:45 +0900

    bench: timout slow ones

--- bench/cnf.hs
+++ bench/cnf.hs
@@ -6,6 +6,7 @@ import Control.DeepSeq (force)
 import Control.Exception (evaluate)
 import Herbrand.Bench
 import Logic.Propositional.Syntax.NormalForm.Classical.Conjunctive
+import Test.Tasty (Timeout (..), localOption)
 
 main :: IO ()
 main = do
@@ -19,9 +20,21 @@ main = do
         , bench "fast" $ nfAppIO (fmap fromFormulaFast) fml
         ]
     , withSats "medium" mediums $ \fml ->
-        [ bench "fast" $ nfAppIO (fmap fromFormulaFast) fml
+        [ localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+            $ bench "naive"
+            $ nfAppIO (fmap fromFormulaNaive) fml
+        , localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+            $ bench "ord"
+            $ nfAppIO (fmap fromFormulaOrd) fml
+        , bench "fast" $ nfAppIO (fmap fromFormulaFast) fml
         ]
     , withSats "large" larges $ \fml ->
-        [ bench "fast" $ nfAppIO (fmap fromFormulaFast) fml
+        [ localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+            $ bench "naive"
+            $ nfAppIO (fmap fromFormulaNaive) fml
+        , localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+            $ bench "ord"
+            $ nfAppIO (fmap fromFormulaOrd) fml
+        , bench "fast" $ nfAppIO (fmap fromFormulaFast) fml
         ]
     ]
