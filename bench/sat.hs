ceeca1c370cf05c858809997967fc3da4ac63f08
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-14 14:00:55 +0900

    Huge benchmarks

--- bench/sat.hs
+++ bench/sat.hs
@@ -7,6 +7,7 @@ import Control.Exception (evaluate)
 import Herbrand.Bench
 import qualified Logic.Propositional.Classical.SAT.DPLL as DPLL
 import qualified Logic.Propositional.Classical.SAT.Tableaux as Tableaux
+import System.Mem (performGC)
 import Test.Tasty (Timeout (..), localOption)
 
 main :: IO ()
@@ -15,6 +16,8 @@ main = do
   !smalls <- evaluate . force =<< findCnfsIn "data/sat/small"
   !mediums <- evaluate . force =<< findCnfsIn "data/sat/medium"
   !larges <- evaluate . force =<< findCnfsIn "data/sat/large"
+  !huges <- evaluate . force =<< findCnfsIn "data/sat/huge"
+  performGC
   defaultMain
     [ bgroup
         "solve"
@@ -34,6 +37,13 @@ main = do
             , bench "DPLL" $ nfAppIO (fmap $ DPLL.solve . fst) fml
             ]
         , withCnfs "large" larges $ \fml ->
+            [ allowFailureBecause "O(n^2)"
+                $ localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+                $ bench "tableaux"
+                $ nfAppIO (fmap $ Tableaux.solve . snd) fml
+            , bench "DPLL" $ nfAppIO (fmap $ DPLL.solve . fst) fml
+            ]
+        , withCnfs "huge" huges $ \fml ->
             [ allowFailureBecause "O(n^2)"
                 $ localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
                 $ bench "tableaux"
