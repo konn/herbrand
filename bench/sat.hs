be520508cfd10c7d0eab35fa9ad88b1c7b07d958
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-14 13:48:39 +0900

    bench: SAT solvers

--- bench/sat.hs
+++ bench/sat.hs
@@ -1,11 +1,39 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main (main) where
 
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
 import Herbrand.Bench
-import Logic.Propositional.Classical.SAT.DPLL
-import Logic.Propositional.Classical.SAT.Tableaux
+import qualified Logic.Propositional.Classical.SAT.DPLL as DPLL
+import qualified Logic.Propositional.Classical.SAT.Tableaux as Tableaux
 import Test.Tasty (Timeout (..), localOption)
 
 main :: IO ()
-main = putStrLn "Not implemented yet"
+main = do
+  !smalls <- evaluate . force =<< findSatsIn "data/sat/small"
+  !mediums <- evaluate . force =<< findSatsIn "data/sat/medium"
+  !larges <- evaluate . force =<< findSatsIn "data/sat/large"
+  defaultMain
+    [ bgroup
+        "solve"
+        [ withCnfs "small" smalls $ \fml ->
+            [ bench "tableaux" $ nfAppIO (fmap $ Tableaux.solve . snd) fml
+            , bench "DPLL" $ nfAppIO (fmap $ DPLL.solve . fst) fml
+            ]
+        , withCnfs "medium" mediums $ \fml ->
+            [ allowFailureBecause "O(n^2)"
+                $ localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+                $ bench "tableaux"
+                $ nfAppIO (fmap $ Tableaux.solve . snd) fml
+            , bench "DPLL" $ nfAppIO (fmap $ DPLL.solve . fst) fml
+            ]
+        , withCnfs "large" larges $ \fml ->
+            [ allowFailureBecause "O(n^2)"
+                $ localOption (Timeout (10 * 10 ^ (6 :: Int)) "10s")
+                $ bench "tableaux"
+                $ nfAppIO (fmap $ Tableaux.solve . snd) fml
+            , bench "DPLL" $ nfAppIO (fmap $ DPLL.solve . fst) fml
+            ]
+        ]
+    ]
