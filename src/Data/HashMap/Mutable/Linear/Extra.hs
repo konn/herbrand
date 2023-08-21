a0b581e22d3080810f50f04a482a4956cfa54731
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-21 20:25:15 +0900

    fix: corrects linearly allocators

--- src/Data/HashMap/Mutable/Linear/Extra.hs
+++ src/Data/HashMap/Mutable/Linear/Extra.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 module Data.HashMap.Mutable.Linear.Extra (
   lookups,
@@ -13,15 +15,17 @@ module Data.HashMap.Mutable.Linear.Extra (
 ) where
 
 import Data.Alloc.Linearly.Token
+import qualified Data.Array.Mutable.Linear.Extra as Array
 import qualified Data.Array.Polarized.Push as Push
 import Data.HashMap.Mutable.Linear
+import Data.HashMap.Mutable.Linear.Internal
 import Data.Hashable (Hashable)
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import GHC.Stack (HasCallStack)
 import Prelude.Linear hiding (lookup)
-import qualified Unsafe.Linear as Unsafe
 import Prelude hiding (lookup, mempty, seq, ($), (.))
+import qualified Prelude as P
 
 lookups ::
   forall key v.
@@ -40,7 +44,16 @@ lookups = go mempty
             go (Push.snoc m arr) (U.unsafeTail uv) hm
 
 emptyL :: (Keyed k) => Linearly %1 -> Int -> HashMap k v
-emptyL l n = l `lseq` unur (empty n (Unsafe.toLinear Ur))
+emptyL l size =
+  let cap = P.max 1 size
+   in HashMap 0 cap (Array.allocL l cap Nothing)
 
 fromListL :: (Keyed k) => Linearly %1 -> [(k, v)] -> HashMap k v
-fromListL l n = l `lseq` unur (fromList n (Unsafe.toLinear Ur))
+fromListL l (xs :: [(k, v)]) =
+  let cap =
+        P.max
+          1
+          (ceiling @Float @Int (fromIntegral (Prelude.length xs) / constMaxLoadFactor))
+   in insertAll
+        xs
+        (HashMap 0 cap (Array.allocL l cap Nothing))
--- src/Data/Set/Mutable/Linear/Extra.hs
+++ src/Data/Set/Mutable/Linear/Extra.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE LinearTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 module Data.Set.Mutable.Linear.Extra (
   module Data.Set.Mutable.Linear,
@@ -12,13 +14,15 @@ module Data.Set.Mutable.Linear.Extra (
 ) where
 
 import Data.Alloc.Linearly.Token
+import qualified Data.HashMap.Mutable.Linear.Extra as LHM
 import Data.Set.Mutable.Linear
+import Data.Set.Mutable.Linear.Internal
 import Prelude.Linear hiding (lookup)
-import qualified Unsafe.Linear as Unsafe
+import qualified Prelude.Linear as P
 import Prelude hiding (lookup, mempty, seq, ($), (.))
 
 emptyL :: (Keyed a) => Linearly %1 -> Int -> Set a
-emptyL l cap = l `lseq` unur (empty cap (Unsafe.toLinear Ur))
+emptyL l cap = Set (LHM.emptyL l cap)
 
 fromListL :: (Keyed a) => Linearly %1 -> [a] -> Set a
-fromListL l as = l `lseq` unur (fromList as (Unsafe.toLinear Ur))
+fromListL l as = Set (LHM.fromListL l (P.map (,()) as))
