ab662f16e7bd36f4bc768264abc1243471f4b252
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-25 19:06:04 +0900

    Debug exe

--- app/cdcl-dry/Main.hs
+++ app/cdcl-dry/Main.hs
@@ -1,31 +1,16 @@
 module Main (main) where
 
-import Data.Maybe (fromMaybe)
 import Logic.Propositional.Classical.SAT.CDCL
 import Logic.Propositional.Syntax.General
 import qualified Logic.Propositional.Syntax.NormalForm.Classical.Conjunctive as CNF
-import Logic.Propositional.Syntax.Transformation.Claassical (dne)
 
-ce :: Formula Full Int
-ce = neg (neg (neg (neg (neg (neg (neg (neg (neg (neg (neg (⊥))))))))))) /\ neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
+fml :: Formula Full Int
+fml = neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
 
 main :: IO ()
 main = do
-  print $ solve $ CNF [CNFClause ([] :: [Literal Int])]
-  print $ solve $ CNF [CNFClause [Positive (0 :: Int)]]
-  print $ solveVarId $ CNF [CNFClause []]
-  print $ solveVarId $ CNF [CNFClause [Positive 0]]
-  print $ solveVarId $ CNF [CNFClause [Negative 0]]
-  print $ dne ce
-  putStrLn "---------"
-  print ce
-  print $ solve $ CNF.fromFormulaFast ce
-  putStrLn "---------"
-  print $ dne ce
-  print $ solve $ CNF.fromFormulaFast $ fromMaybe ce (dne ce)
-  putStrLn "---------"
-  print $ dne =<< dne ce
-  print $ solve $ CNF.fromFormulaFast $ fromMaybe ce (dne =<< dne ce)
-  putStrLn "---------"
-  print $ dne =<< dne =<< dne ce
-  print $ solve $ CNF.fromFormulaFast $ fromMaybe ce (dne =<< dne =<< dne ce)
+  print fml
+  let cnf = CNF.fromFormulaFast fml
+  print cnf
+
+  print $ solve cnf
