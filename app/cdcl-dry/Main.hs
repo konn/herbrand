d639c9d4d95144752c86e9e8821e5a4961dcae62
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-25 18:07:15 +0900

    Adds unit test

--- app/cdcl-dry/Main.hs
+++ app/cdcl-dry/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
+import Data.Maybe (fromMaybe)
 import Logic.Propositional.Classical.SAT.CDCL
+import Logic.Propositional.Syntax.General
+import qualified Logic.Propositional.Syntax.NormalForm.Classical.Conjunctive as CNF
+import Logic.Propositional.Syntax.Transformation.Claassical (dne)
+
+ce :: Formula Full Int
+ce = neg (neg (neg (neg (neg (neg (neg (neg (neg (neg (neg (⊥))))))))))) /\ neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
 
 main :: IO ()
 main = do
@@ -8,3 +15,17 @@ main = do
   print $ solve $ CNF [CNFClause [Positive (0 :: Int)]]
   print $ solveVarId $ CNF [CNFClause []]
   print $ solveVarId $ CNF [CNFClause [Positive 0]]
+  print $ solveVarId $ CNF [CNFClause [Negative 0]]
+  print $ dne ce
+  putStrLn "---------"
+  print ce
+  print $ solve $ CNF.fromFormulaFast ce
+  putStrLn "---------"
+  print $ dne ce
+  print $ solve $ CNF.fromFormulaFast $ fromMaybe ce (dne ce)
+  putStrLn "---------"
+  print $ dne =<< dne ce
+  print $ solve $ CNF.fromFormulaFast $ fromMaybe ce (dne =<< dne ce)
+  putStrLn "---------"
+  print $ dne =<< dne =<< dne ce
+  print $ solve $ CNF.fromFormulaFast $ fromMaybe ce (dne =<< dne =<< dne ce)
--- test/Logic/Propositional/Classical/Syntax/TestUtils.hs
+++ test/Logic/Propositional/Classical/Syntax/TestUtils.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Logic.Propositional.Classical.Syntax.TestUtils (
@@ -22,6 +23,7 @@ import Test.Falsify.Predicate ((.$))
 import qualified Test.Falsify.Predicate as P
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Falsify
+import Test.Tasty.HUnit (assertBool, assertFailure, testCase, (@?=))
 
 testProverSemantics ::
   Arity ->
@@ -75,43 +77,86 @@ testSolverSemanticsWith ::
 testSolverSemanticsWith projVar toInput vs sz solver =
   testGroup
     "behaves sematically correctly"
-    [ testProperty "Gives a correct decision" $ do
-        (phi, consis) <- genFormula vs sz
-        let inp = toInput phi
-        info $ "Direct Input: " <> show inp
-        let ans = solver inp
-        case consis of
-          Inconsistent ->
-            assert
-              $ P.eq
-              .$ ("expected", Unsat)
-              .$ ("answer", projModel projVar <$> ans)
-          resl ->
-            assert
-              $ P.satisfies
-                ("Satisfiable (" <> show resl <> ")", \case Satisfiable {} -> True; _ -> False)
-              .$ ("answer", projModel projVar <$> ans)
-    , testProperty "Gives a correct model" $ do
-        (phi, consis) <- genFormula vs sz
-        label
-          "consistency"
-          [ case consis of
-              Tautology -> "Tautology"
-              Consistent {} -> "Satsifiable"
-              Inconsistent -> "Unsatisfiable"
-          ]
-        let phi' = case consis of
-              Inconsistent -> neg phi
-              _ -> phi
-        info $ "Satsifiable Formula: " <> show phi'
-        let inp = toInput phi'
-        info $ "Direct Input: " <> show inp
-        case solver inp of
-          Unsat -> testFailed "Expected Satisfiable, but got Unsat"
-          Satisfiable m -> do
-            info $ "Given model: " <> show m
-            assert
-              $ P.eq
-              .$ ("expected", Just True)
-              .$ ("answer", eval (projModel projVar m) phi')
+    [ testGroup
+        "Gives a correct decision"
+        [ testProperty "Random" $ do
+            (phi, consis) <- genFormula vs sz
+            let inp = toInput phi
+            info $ "Direct Input: " <> show inp
+            let ans = solver inp
+            case consis of
+              Inconsistent ->
+                assert
+                  $ P.eq
+                  .$ ("expected", Unsat)
+                  .$ ("answer", projModel projVar <$> ans)
+              resl ->
+                assert
+                  $ P.satisfies
+                    ("Satisfiable (" <> show resl <> ")", \case Satisfiable {} -> True; _ -> False)
+                  .$ ("answer", projModel projVar <$> ans)
+        , testGroup
+            "Regressions"
+            [ testCase "show phi" do
+              let consis = classifyFormula phi
+                  ans = solver $ toInput phi
+              case consis of
+                Inconsistent ->
+                  (projModel projVar <$> ans) @?= Unsat
+                resl ->
+                  assertBool
+                    ("Must be satisfiable (" <> show resl <> "), but got: " <> show ans)
+                    $ case projModel projVar <$> ans of
+                      Satisfiable {} -> True
+                      _ -> False
+            | phi <- testFormulae
+            ]
+        ]
+    , testGroup
+        "Gives a correct model"
+        [ testProperty "Random" $ do
+            (phi, consis) <- genFormula vs sz
+            label
+              "consistency"
+              [ case consis of
+                  Tautology -> "Tautology"
+                  Consistent {} -> "Satsifiable"
+                  Inconsistent -> "Unsatisfiable"
+              ]
+            let phi' = case consis of
+                  Inconsistent -> neg phi
+                  _ -> phi
+            info $ "Satsifiable Formula: " <> show phi'
+            let inp = toInput phi'
+            info $ "Direct Input: " <> show inp
+            case solver inp of
+              Unsat -> testFailed "Expected Satisfiable, but got Unsat"
+              Satisfiable m -> do
+                info $ "Given model: " <> show m
+                assert
+                  $ P.eq
+                  .$ ("expected", Just True)
+                  .$ ("answer", eval (projModel projVar m) phi')
+        , testGroup
+            "Regressions"
+            [ testCase "show phi" do
+              let consis = classifyFormula phi
+                  phi' = case consis of
+                    Inconsistent -> neg phi
+                    _ -> phi
+              case solver $ toInput phi' of
+                Unsat -> assertFailure "Expected Satisfiable, but got Unsat"
+                Satisfiable m -> do
+                  eval (projModel projVar m) phi' @?= Just True
+            | phi <- testFormulae
+            ]
+        ]
     ]
+
+testFormulae :: [Formula Full Int]
+testFormulae =
+  [ neg (⊥) /\ neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
+  , neg (neg (neg (⊥))) /\ neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
+  , neg (neg (neg (neg (neg (⊥))))) /\ neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
+  , neg (neg (neg (neg (neg (neg (neg (neg (neg (neg (neg (⊥))))))))))) /\ neg ((neg (⊥) ==> (⊤) /\ Atom 0) /\ (neg (⊥) ==> (⊥) \/ Atom 0))
+  ]
