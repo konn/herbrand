02b3504555e54f83bec2d2f197866a741f507f2e
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-26 12:17:50 +0900

    ci: factors out key generation

---
+++ ci/generate-cache-key.js
@@ -0,0 +1,46 @@
+function build_keys(comps) {
+  const fallbacks = comps
+    .reduce((accum, cur) => [accum[0].concat([cur])].concat(accum), [[]])
+    .slice(1, -1)
+    .map((e) => e.concat("").join("-"));
+
+  return { key: comps.join("-"), restore: fallbacks.join("\n") };
+}
+
+module.exports = async ({ core, glob, os, ghc, projects, prefix }) => {
+  const prefix = prefix === null ? "" : `${prefix}-`;
+  const projects =
+    projects === null
+      ? ["cabal.project", "cabal.project", "cabal.project.freeze"]
+      : projects;
+  const project_hash = await glob.hashFiles(projects);
+  core.setOutput("project", project_hash);
+
+  const package_hash = await glob.hashFiles("**/package.yaml", "**/*.cabal");
+  core.setOutput("package", package_hash);
+
+  const source_hash = await glob.hashFiles(
+    "**/*.hs",
+    "**/*.lhs",
+    "**/*.hsig",
+    "**/*.hs-boot",
+    "**/*.c",
+    "**/*.h",
+    "**/*.chs",
+    "**/*.hsc"
+  );
+  core.setOutput("source", source_hash);
+
+  const store_prefix = `${prefix}store-${os}-${ghc}`;
+  core.setOutput("store-prefix", store_prefix);
+  const store_keys = build_keys([store_prefix, project_hash, package_hash]);
+  core.setOutput("store", store_keys.key);
+  core.setOutput("store-restore", store_keys.restore);
+
+  const dist_prefix = `${prefix}dist-${os}-${ghc}`;
+  core.setOutput("dist-prefix", dist_prefix);
+  const dist_key_comps = [dist_prefix, project_hash, package_hash, source_hash];
+  const dist_keys = build_keys(dist_key_comps);
+  core.setOutput("dist", dist_keys.key);
+  core.setOutput("dist-restore", dist_keys.restore);
+};
