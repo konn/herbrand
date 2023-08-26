92186584ef9b38c1953dfe6b1da9aba582abc335
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-26 12:37:53 +0900

    stop bad logging

--- ci/generate-cache-key.js
+++ ci/generate-cache-key.js
@@ -14,10 +14,6 @@ module.exports = async ({ core, glob, os, ghc, projects, prefix }) => {
 
   prefix = prefix == null ? "" : `${prefix}-`;
   core.info(`New prefix: ${JSON.stringify(prefix)}`);
-  projects =
-    projects == null
-      ? ["cabal.project", "cabal.project", "cabal.project.freeze"]
-      : projects;
   core.info(`New projects: ${JSON.stringify(projects)}`);
   const project_hash = await glob.hashFiles(...projects);
   core.info(`project_hash: ${JSON.stringify(project_hash)}`);
@@ -37,25 +33,18 @@ module.exports = async ({ core, glob, os, ghc, projects, prefix }) => {
     "**/*.chs",
     "**/*.hsc"
   );
-  core.info(`source_hash: ${JSON.stringify(source_hash)}`);
   core.setOutput("source", source_hash);
 
   const store_prefix = `${prefix}store-${os}-${ghc}`;
-  core.info(`store_prefix: ${JSON.stringify(store_prefix)}`);
   core.setOutput("store-prefix", store_prefix);
   const store_keys = build_keys([store_prefix, project_hash, package_hash]);
   core.setOutput("store", store_keys.key);
   core.setOutput("store-restore", store_keys.restore);
-  core.info(`store: ${JSON.stringify(store)}`);
-  core.info(`store-restore: ${JSON.stringify(store_restore)}`);
 
   const dist_prefix = `${prefix}dist-${os}-${ghc}`;
-  core.info(`dist_prefix: ${JSON.stringify(dist_prefix)}`);
   core.setOutput("dist-prefix", dist_prefix);
   const dist_key_comps = [dist_prefix, project_hash, package_hash, source_hash];
   const dist_keys = build_keys(dist_key_comps);
   core.setOutput("dist", dist_keys.key);
   core.setOutput("dist-restore", dist_keys.restore);
-  core.info(`dist: ${JSON.stringify(store)}`);
-  core.info(`dist-restore: ${JSON.stringify(dist_restore)}`);
 };
