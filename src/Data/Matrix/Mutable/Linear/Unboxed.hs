4c06853b161607f7e559a8bd88d50fe7f896e66b
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-27 19:58:52 +0900

    Unboxed matrix

---
+++ src/Data/Matrix/Mutable/Linear/Unboxed.hs
@@ -0,0 +1,140 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+
+module Data.Matrix.Mutable.Linear.Unboxed (
+  Matrix (),
+  emptyL,
+  fromRowL,
+  fromRowsL,
+  numRows,
+  numEntries,
+  pushRow,
+  getRow,
+  unsafeGetRow,
+  popRow,
+  dropRowsEnd,
+  shrinkToFit,
+) where
+
+import Data.Alloc.Linearly.Token (Linearly, besides)
+import qualified Data.Array.Polarized as PV
+import qualified Data.Array.Polarized.Pull.Extra as Pull
+import qualified Data.Array.Polarized.Push.Extra as Push
+import qualified Data.List as List
+import qualified Data.Unrestricted.Linear as Ur
+import qualified Data.Vector.Mutable.Linear.Unboxed as LUV
+import qualified Data.Vector.Unboxed as U
+import GHC.Stack (HasCallStack)
+import Prelude.Linear
+import qualified Prelude as P
+
+data Matrix a where
+  Matrix ::
+    {-# UNPACK #-} !(LUV.Vector Int) %1 ->
+    !(LUV.Vector a) %1 ->
+    Matrix a
+
+instance Consumable (Matrix a) where
+  consume (Matrix c p) = c `lseq` consume p
+  {-# INLINE consume #-}
+
+{- HLINT ignore module "Use bimap" -}
+instance (U.Unbox a) => Dupable (Matrix a) where
+  {-# INLINE dup2 #-}
+  dup2 (Matrix c p) =
+    dup2 c & \(c, c') ->
+      dup2 p & \(p, p') ->
+        (Matrix c p, Matrix c' p')
+
+emptyL :: (U.Unbox a) => Linearly %1 -> Matrix a
+{-# INLINE emptyL #-}
+emptyL l =
+  besides l (\l -> LUV.constantL l 1 0) & \(offsets, l) ->
+    LUV.emptyL l & Matrix offsets
+
+fromRowL :: (U.Unbox a) => Linearly %1 -> U.Vector a %1 -> Matrix a
+{-# INLINE fromRowL #-}
+fromRowL l uv =
+  besides l (`LUV.fromVectorL` uv) & \(payload, l) ->
+    LUV.size payload & \(Ur sz, payload) ->
+      LUV.fromListL l [0, sz] & \offsets -> Matrix offsets payload
+
+fromRowsL :: (U.Unbox a) => Linearly %1 -> [U.Vector a] -> Matrix a
+{-# INLINE fromRowsL #-}
+fromRowsL l rows =
+  besides
+    l
+    (`LUV.fromListL` List.scanl' (flip $ (P.+) P.. U.length) 0 rows)
+    & \(offs, l) ->
+      Matrix offs
+        $ LUV.fromVectorL l
+        $ Push.alloc
+        $ foldMap' (PV.transfer . Pull.fromVector) rows
+
+numRows :: Matrix a %1 -> (Ur Int, Matrix a)
+{-# INLINE numRows #-}
+numRows (Matrix offsets payload) =
+  LUV.size offsets & \(Ur offSz, offsets) ->
+    (Ur (offSz - 1), Matrix offsets payload)
+
+numEntries :: Matrix a %1 -> (Ur Int, Matrix a)
+{-# INLINE numEntries #-}
+numEntries (Matrix offsets payload) =
+  LUV.size payload & \(Ur len, payload) ->
+    (Ur len, Matrix offsets payload)
+
+pushRow :: (U.Unbox a) => U.Vector a -> Matrix a %1 -> Matrix a
+{-# INLINE pushRow #-}
+pushRow uv mat =
+  numEntries mat & \(Ur oldOffs, Matrix offsets payload) ->
+    Matrix
+      (LUV.push (U.length uv + oldOffs) offsets)
+      (LUV.appendVector uv payload)
+
+-- | Pop the last row of the matrix. This will never shrink the vector, use shrinkToFit to remove the wasted space.
+popRow :: (U.Unbox a) => Matrix a %1 -> (Ur (Maybe (U.Vector a)), Matrix a)
+popRow mat =
+  numEntries mat & \(Ur origEnts, Matrix offs ents) ->
+    LUV.pop offs & \(Ur mlen, offs) ->
+      mlen & \case
+        Nothing -> (Ur Nothing, Matrix offs ents)
+        Just len ->
+          LUV.slice' (origEnts - len) len ents & \(ents, popped) ->
+            (Ur.lift Just (LUV.freeze popped), Matrix offs ents)
+
+-- |  Retrievies a row copying.
+unsafeGetRow :: (U.Unbox a) => Int -> Matrix a %1 -> (Ur (U.Vector a), Matrix a)
+{-# INLINE unsafeGetRow #-}
+unsafeGetRow i (Matrix offs ents) =
+  LUV.unsafeGet i offs & \(Ur start, offs) ->
+    LUV.unsafeGet (i + 1) offs & \(Ur end, offs) ->
+      LUV.slice' start (end - start) ents & \(ents, uvSeed) ->
+        (LUV.freeze uvSeed, Matrix offs ents)
+
+getRow :: (HasCallStack, U.Unbox a) => Int -> Matrix a %1 -> (Ur (U.Vector a), Matrix a)
+{-# INLINE getRow #-}
+getRow i mat =
+  numRows mat & \(Ur n, mat) ->
+    i < n & \case
+      True -> unsafeGetRow i mat
+      False -> error ("getRow: row index out of bound: " <> show (i, n)) mat
+
+-- | shrinkToFit
+shrinkToFit :: (U.Unbox a) => Matrix a %1 -> Matrix a
+shrinkToFit (Matrix off pay) = Matrix (LUV.shrinkToFit off) (LUV.shrinkToFit pay)
+
+dropRowsEnd :: (U.Unbox a) => Int -> Matrix a %1 -> Matrix a
+{-# INLINE dropRowsEnd #-}
+dropRowsEnd n mat =
+  numRows mat & \(Ur nrows, Matrix offs ents) ->
+    n >= nrows & \case
+      True -> Matrix (LUV.slice 0 0 offs) (LUV.slice 0 0 ents)
+      False ->
+        LUV.unsafeGet n offs & \(Ur k, offs) ->
+          Matrix (LUV.slice 0 n offs) (LUV.slice 0 k ents)
