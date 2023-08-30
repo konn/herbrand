6e3f3ecff7e8ad260b69a5f0ef2523dbbf0042bc
Hiromi ISHII <konn.jinro@gmail.com>
2023-08-30 23:27:14 +0900

    option to show solution

--- app/cdcl-dry/Main.hs
+++ app/cdcl-dry/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -9,18 +10,20 @@ import Control.Applicative ((<**>))
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
 import Control.Lens
-import Control.Monad (void)
 import qualified Data.ByteString.Lazy as LBS
 import Logic.Propositional.Classical.SAT.CDCL
 import Logic.Propositional.Classical.SAT.Format.DIMACS (parseCNFLazy)
 import qualified Options.Applicative as Opt
 
-newtype Opt = Opt {input :: FilePath}
+data Opt = Opt {input :: FilePath, detail :: Bool}
 
 optP :: Opt.ParserInfo Opt
 optP = Opt.info (p <**> Opt.helper) $ Opt.progDesc "Solves SAT problem on CNF formula by CDCL"
   where
-    p = Opt <$> Opt.strOption (Opt.long "input" <> Opt.short 'i' <> Opt.metavar "PATH" <> Opt.help "The path to the input CNF file.")
+    p = do
+      input <- Opt.strOption (Opt.long "input" <> Opt.short 'i' <> Opt.metavar "PATH" <> Opt.help "The path to the input CNF file.")
+      detail <- Opt.switch $ Opt.long "detail" <> Opt.short 'D' <> Opt.help "Shows solution"
+      pure Opt {..}
 
 cnf0, cnf1 :: CNF VarId
 cnf0 = [[Positive 1, Negative 0, Positive 1, Positive 1, Positive 1]]
@@ -36,4 +39,6 @@ main = do
   putStrLn "CNF evaluated. Solving..."
 
   resl <- evaluate $ force $ solve cnf
-  print $ () <$ resl
+  if detail
+    then print resl
+    else print $ () <$ resl
